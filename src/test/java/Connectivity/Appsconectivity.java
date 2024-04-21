791028aeeef18410c05f6788acefc139af65c2a9
Rohit <rohitrajpoot692@gmail.com>
2024-04-21 18:52:26 +0530

    AppsConnect

---
+++ src/test/java/Connectivity/Appsconectivity.java
@@ -0,0 +1,11 @@
+package Connectivity;
+
+import org.testng.annotations.Test;
+
+public class Appsconectivity {
+
+	@Test
+	public void appsConnectivity() {
+		System.out.println("apps connect");
+	}
+}
